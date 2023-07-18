8cb8275949316ed0edd3e786ef26eae2c2a0520f
Adam Masters <74498986+acmasters17@users.noreply.github.com>
2023-07-18 16:22:00 +0100

    Kit Screen V1 Implemented

--- screens/KitScreen.tsx
+++ screens/KitScreen.tsx
@@ -6,10 +6,15 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { sanitizeLabName } from "../utilities/sanitizer";
 import { throwToastError } from "../utilities/toastFunctions";
 import MaterialCommunityIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import { CheckBox } from "@rneui/base";
 
 export default function KitScreen() {
-  const [kitChecklistItems, setKitChecklistItems] = useState<string[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [kitChecklistItems, setKitChecklistItems] = useState<string[]>([
+    "Test",
+    "Test 2",
+  ]);
+  const [checkedItems, setCheckedItems] = useState<boolean[]>([false, true]);
+  const [isLoading, setIsLoading] = useState(false);
   const functions = getFunctions();
 
   // get all users on start up
@@ -32,8 +37,8 @@ export default function KitScreen() {
           const newKitChecklist = data.kitChecklist as string[];
 
           setKitChecklistItems(newKitChecklist);
+          setCheckedItems(newKitChecklist.map(_ => false));
 
-          console.log(req.data);
         } catch (e) {
           throwToastError(e);
         }
@@ -64,23 +69,62 @@ export default function KitScreen() {
       <Text
         category="s1"
         style={{
-          marginTop: 10,
+          marginVertical: 10,
         }}
       >
         Make sure you've got everything before you go
       </Text>
-      <View>
-        <Text>Items</Text>
-        <TouchableOpacity>
-          <View>
-            <MaterialCommunityIcon name="eraser"/>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          padding: 20,
+          justifyContent: "space-between",
+          width: "100%",
+          alignItems: "center",
+        }}
+      >
+        <Text category="h6">Items</Text>
+        <TouchableOpacity onPress={() => setCheckedItems([false,false])}>
+          <View
+            style={{
+              padding: 10,
+              backgroundColor: "#0C2962",
+              borderRadius: 10,
+            }}
+          >
+            <MaterialCommunityIcon name="eraser" color="white" size={20} />
           </View>
         </TouchableOpacity>
       </View>
       {kitChecklistItems.length === 0 ? (
-        <Text>No items have been set yet!</Text>
+        <Text style={{textAlign: "center", margin: 20}}>No items have been set yet! Please contact your head of lab to add items.</Text>
       ) : (
-        kitChecklistItems.map((item, key) => <Text key={key}>{item}</Text>)
+        kitChecklistItems.map((item, key) => (
+          <View
+            key={key}
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              width: "100%",
+              justifyContent: "space-between",
+              alignItems:"center",
+              paddingHorizontal: 20,
+              paddingVertical: 2
+            }}
+          >
+            <Text category="s1">{item}</Text>
+            <CheckBox
+              checked={checkedItems[key]}
+              onPress={() => setCheckedItems(oldCheckedItems => oldCheckedItems.map((item, index) => index === key ? !oldCheckedItems[key] : item))}
+              // Use ThemeProvider to make change for all checkbox
+              iconType="material-community"
+              checkedIcon="checkbox-marked"
+              uncheckedIcon="checkbox-blank-outline"
+              checkedColor="#419488"
+            />
+          </View>
+        ))
       )}
     </Layout>
   );
