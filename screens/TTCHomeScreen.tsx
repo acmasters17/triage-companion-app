1c1e3c5e8292a36fee79ae13d6275d0097bf0e34
Adam Masters <74498986+acmasters17@users.noreply.github.com>
2023-07-25 16:20:21 +0100

    Backgrounds Sorted

--- screens/TTCHomeScreen.tsx
+++ screens/TTCHomeScreen.tsx
@@ -1,9 +1,21 @@
-import { StyleSheet, View, TouchableOpacity, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+  ImageBackground,
+} from "react-native";
 import React from "react";
 import { Button, Layout, Spinner, Text } from "@ui-kitten/components";
 import { Category } from "../utilities/categoriesModel";
 import { ParamListBase, useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+const laptopBackground = require("../assets/laptop-card-background.png");
+const smartphoneBackground = require("../assets/smartphone-card-background.png");
+const tabletBackground = require("../assets/tablet-card-background.png");
+const otherBackground = require("../assets/other-card-background.png");
 
 type TTCScreenProps = {
   isLoading: boolean;
@@ -13,6 +25,20 @@ type TTCScreenProps = {
 
 export default function TTCHomeScreen(props: TTCScreenProps) {
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+
+  const getBackground = (categoryName: string) => {
+    const cleanedCategoryName = categoryName.replace(/\s+/g, "").toLowerCase();
+    switch (cleanedCategoryName) {
+      case "laptop":
+        return laptopBackground;
+      case "smartphone":
+        return smartphoneBackground;
+      case "tablet":
+        return tabletBackground;
+      default:
+        return otherBackground;
+    }
+  };
 
   if (props.isLoading)
     return (
@@ -48,17 +74,43 @@ export default function TTCHomeScreen(props: TTCScreenProps) {
           add items.
         </Text>
       ) : (
-        <ScrollView style={{ marginTop: 20, marginLeft: 4 }}>
+        <ScrollView style={{ marginTop: 20, marginHorizontal: 20 }}>
           {props.categories.map((item, key) => (
-            <Button
+            <TouchableOpacity
               key={key}
               onPress={() => {
                 props.setChosenCategoryIndex(key);
                 navigation.navigate("TTCCategory");
               }}
+              style={{ marginVertical: 10 }}
             >
-              {item.categoryName}
-            </Button>
+              <ImageBackground
+                source={getBackground(item.categoryName)}
+                style={{ width: "100%", height: 150, borderRadius: 20 }}
+                resizeMode="cover"
+                imageStyle={{ borderRadius: 20 }}
+                blurRadius={3}
+              >
+                <View
+                  style={{
+                    backgroundColor: "white",
+                    position: "absolute",
+                    bottom: 0,
+                    width: "100%",
+                    shadowColor: "#171717",
+                    shadowOffset: { width: -2, height: 4 },
+                    shadowOpacity: 0.2,
+                    shadowRadius: 3,
+                    borderBottomLeftRadius: 20,
+                    borderBottomRightRadius: 20,
+                  }}
+                >
+                  <Text style={{ padding: 8 }} category="s1">
+                    {item.categoryName}
+                  </Text>
+                </View>
+              </ImageBackground>
+            </TouchableOpacity>
           ))}
         </ScrollView>
       )}
